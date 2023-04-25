36be6c70459150afc5ff8ecd34072aa8e07ca47e
anna-sahradyan <a.sahradyan72@gmail.com>
2023-04-25 12:56:58 +0400

    start deploy

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import postRouter from './routes/posts-router.js';
+import userRouter from "./routes/user-router.js";
+import path from "path";
+//?Constants
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+dotenv.config();
+
+
+//?Middleware
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+//?Routes
+app.use(`/posts`, postRouter);
+app.use(`/user`,userRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__filename, "../frontendmedia/build")))
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../", "frontendmedia", "build", "index.html")
+        )
+    })
+
+} else {
+    app.get("/", (req, res) => {
+        res.send("Home Page")
+    })
+
+}
+
+//?Connect
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => app.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`))).catch((error) => console.log(error.message));
+
+
+
